3f89a72a53f4edb96e110bc8da5223d043feaca9
Gary Love <garylove@gmail.com>
2020-06-15 17:07:48 -0700

    Keep custom domain set

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,5 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('images');
   eleventyConfig.addPassthroughCopy('css');
   eleventyConfig.addPassthroughCopy('js');
+  eleventyConfig.addPassthroughCopy('CNAME');
 };
